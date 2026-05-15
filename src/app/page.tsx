73b59537c6c9cddd6fa1f8beb29460cb80d060fd
zane <czl970721@gmail.com>
2026-05-15 17:06:25 -0400

    scriptize map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import { Map, MapControls } from "@/components/ui/map"
+import { getMapStyle } from "@/lib/map-style"
 
 export default function Page() {
   return (
     <div className="w-full h-full">
-      <Map>
+      <Map styles={{ light: getMapStyle("light"), dark: getMapStyle("dark") }}>
         <MapControls
           className="right-2 bottom-16"
           showZoom
