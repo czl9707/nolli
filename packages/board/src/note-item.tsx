a3dd57733dd0705e1ae007d1896723b3c9b9d696
zane <czl970721@gmail.com>
2026-08-16 21:48:47 -0400

    feat(board): diegetic notes + photo captions in playful; note editor WYSIWYG

--- packages/board/src/note-item.tsx
+++ packages/board/src/note-item.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import type { PlacedArchItem } from "./pin-board-layout"
-import { Body2 } from "@nolli/ui"
+import { Note } from "@nolli/ui"
 import { BoardItem } from "./board-item"
 import { BoardModal } from "./board-modal"
 import styles from "./note-item.module.css"
@@ -21,11 +21,11 @@ export function NoteItem({ note, position, delay }: NoteItemProps) {
         className={styles.note}
         onClick={() => setOpen(true)}
       >
-        <Body2>{note.text}</Body2>
+        <Note>{note.text}</Note>
       </BoardItem>
       <BoardModal open={open} onClose={() => setOpen(false)}>
         <div className={`${styles.note} ${styles.modalNote}`}>
-          <Body2>{note.text}</Body2>
+          <Note>{note.text}</Note>
         </div>
       </BoardModal>
     </>
